29fe3819221ccdef8a63eb864328aca2ec721abc
iqFareez <60868965+fareezMaple@users.noreply.github.com>
2020-12-08 23:49:23 +0800

    first commit

---
+++ example/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:malaysia_prayer_time/malaysia_prayer_time.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MPT example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(body: MyExampleApp(),),
+    );
+  }
+}
+
+class MyExampleApp extends StatefulWidget {
+  @override
+  _MyExampleAppState createState() => _MyExampleAppState();
+}
+
+class _MyExampleAppState extends State<MyExampleApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(MTT),
+    );
+  }
+}
+
---
+++ lib/malaysia_prayer_time.dart
@@ -0,0 +1,9 @@
+library malaysia_prayer_time;
+
+/// A Malaysia Prayer Time class
+class MPT {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+
+  static String get welcomeString => 'Hi to this plugins';
+}
